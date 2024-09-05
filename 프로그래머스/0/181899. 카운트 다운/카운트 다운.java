8ce395cd3a8928a00aaf8ed03e0e6ac78c7f0cef
jieun <79819640+jjie47@users.noreply.github.com>
2024-09-05 10:00:18 +0900

    [level 0] Title: 카운트 다운, Time: 0.03 ms, Memory: 70.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181899. 카운트 다운/카운트 다운.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int[] solution(int start_num, int end_num) {
+        int count = start_num-end_num+1;
+        int[] answer = new int[count];
+        for(int i=0; i<count; i++){
+            answer[i] = start_num-i;
+        }
+        return answer;
+    }
+}
